5c914bc14baaafeefd6a7bf2fc7c1637c8a59211
Elisapdx <94479420+Elisapdx@users.noreply.github.com>
2022-12-08 16:54:07 +0100

    Update ft_put_hex.c

--- ft_put_hex.c
+++ ft_put_hex.c
@@ -1,11 +1,4 @@
-int	ft_put_hex(unsigned int nb, const char form)
-{
-	if (nb == 0)
-		write(1, "0", 1);
-	else
-		ft_print_hex(nb, form);
-	return (ft_hex_len(nb));
-}
+#include "ft_printf.h"
 
 int	ft_hex_len(unsigned	int nb)
 {
@@ -20,9 +13,18 @@ int	ft_hex_len(unsigned	int nb)
 	return (len);
 }
 
+int	ft_put_hex(unsigned int nb, char *base, const char form)
+{
+	if (nb == 0)
+		write(1, "0", 1);
+	else
+		ft_print_hex(nb, base, form);
+	return (ft_hex_len(nb));
+}
+
 void	ft_print_hex(unsigned int nb, char *base, const char form)
 {
 	if (nb >= 16)
-		ft_print_hex(nb / 16);
+		ft_print_hex(nb / 16, base, form);
 	ft_putchar(base[nb % 16]);
 }
