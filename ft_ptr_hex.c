34d8ec4be9c00c0c93220053e101938d88b48a90
Elisapdx <94479420+Elisapdx@users.noreply.github.com>
2022-12-09 11:43:04 +0100

    Update ft_ptr_hex.c

--- ft_ptr_hex.c
+++ ft_ptr_hex.c
@@ -1,18 +1,5 @@
 #include "ft_printf.h"
 
-int	ft_ptr_len(unsigned	int nb)
-{
-	int	len;
-
-	len = 0;
-	while (nb != 0)
-	{
-		len++;
-		nb = nb / 16;
-	}
-	return (len);
-}
-
 void	ft_ptr_hex(unsigned long nb, char *base, int *len)
 {
 	if (nb >= 16)
@@ -21,15 +8,12 @@ void	ft_ptr_hex(unsigned long nb, char *base, int *len)
 	(*len)++;
 }
 
-int	ft_print_ptr(unsigned long long ptr)
+int	ft_print_ptr(unsigned long ptr)
 {
 	int	len;
 
 	len = 0;
-	len += write(1, "0x", 2);
-	if (ptr == 0)
-		len += write(1, "0", 1);
-	else
-		ft_ptr_hex(ptr, "0123456789abcdef", len);
+	len += ft_putstr("0x");
+	ft_ptr_hex(ptr, "0123456789abcdef", &len);
 	return (len);
 }
