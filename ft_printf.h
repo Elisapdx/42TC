f6993172f0af888a23c519ff7f071baa4b0f613e
Elisapdx <94479420+Elisapdx@users.noreply.github.com>
2022-12-09 15:52:05 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -21,7 +21,7 @@ char	*ft_itoa(int n);
 int	ft_ptr_hex(unsigned int nb, char *base, int *len);
 int	ft_print_ptr(unsigned long ptr);
 int	ft_hex_len(unsigned	int nb);
-void	ft_put_hex(unsigned int nb, char *base);
+int	ft_put_hex(unsigned int nb, char *base);
 void	ft_print_hex(unsigned int nb, char *base, const char form);
 void  ft_conv_hex(long nb, char *base, int *len);
 
